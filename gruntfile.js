1aa4e1f87bbb45396249202ace420d83decf0d00
Ryan silva <ryanrocke2004@gmail.com>
2024-03-24 13:37:34 -0300

    novo commit

---
+++ gruntfile.js
@@ -0,0 +1,82 @@
+const { option } = require("grunt");
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less:{
+                files:['src/styles/**/*.less'],
+                tasks:['less:development']
+            }
+        },
+        replace:{
+            dev:{
+                options:{
+                    patterns:[
+                        {
+                            match:'ENDERECO_DO_CSS',
+                            replacement:'./styles/main.css'
+                        },
+                    {
+                        match:'ENDERECO_DO_JS',
+                        replacement:'../src/scripts/main.js'
+                    }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten:true,
+                        src:['src/index.html'],
+                        dest:'dev/'
+                    }
+                ]
+            },
+        
+        },
+        htmlmin:{
+            dist:{
+                options:{
+                    removeComments: true,
+                    collapseWhitespace:true
+                },
+                files:{
+                    'prebuild/index.html':'src/index.html'
+                }
+            }
+        },
+        clean:['prebuild'],
+        uglify:{
+            target:{
+                files:{
+                    'dist/scripts/main.js':'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default',['watch']);
+    grunt.registerTask('build',['less:production', 'html:dist','replace:dist','clean','uglify']);
+}
